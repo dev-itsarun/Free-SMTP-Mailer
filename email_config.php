a9bf8a060ce38d40cfa479c4410c170d9b6d0527
Arun Kumar <dev.itsarun@gmail.com>
2022-10-08 17:21:57 +0530

    test commit

--- email_config.php
+++ email_config.php
@@ -5,7 +5,7 @@ $your_name = "A Mail From WebSite";  // Ex: your Company Name
 $your_gmail = "contact.bloomerzz@gmail.com"; // EX : youremail@gmail.com
 $app_password = "xptnjhxvpkejerbn"; // EX: xxxxxxxxxxxxxxxx  [max 16 digit pass]
 
-$allowed_host = array("http://localhost","https://maaperumsabai.com"); //EX : https://localhost [ to protect your server ]
+$allowed_host = array("http://localhost","http://192.168.0.100","https://maaperumsabai.com"); //EX : https://localhost [ to protect your server ]
 
 $setmail = "dlcloud@gmail.com"
 
